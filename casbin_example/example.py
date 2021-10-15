afc51ff69c4c59792fe3a3763c5d2abeb906e438
yijunxu <yijunx@umich.edu>
2021-10-15 06:10:44 +0000

    added some casbin stuff

---
+++ casbin_example/example.py
@@ -0,0 +1,32 @@
+import casbin_sqlalchemy_adapter
+import casbin
+
+adapter = casbin_sqlalchemy_adapter.Adapter("postgresql://postgres:postgres@db:5432/db")
+
+e = casbin.Enforcer("casbin_example/model.conf", adapter)
+
+
+
+# e.add_policy(
+#     "fff", "data1", "read"
+# )
+
+e.add_policy("reader", "client", "read")
+e.add_policies([("author", "client", "modify"), ("author", "client", "share"), ("admin", "client", "delete")])
+
+e.add_grouping_policy("bob", "reader")
+
+
+print(e.get_permissions_for_user("reader"))
+
+sub = "bob"  # the user that wants to access a resource.
+obj = "client"  # the resource that is going to be accessed.
+act = "read"  # the operation that the user performs on the resource.
+if e.enforce(sub, obj, act):
+    # permit alice to read data1casbin_sqlalchemy_adapter
+    print("pass")
+    pass
+else:
+    # deny the request, show an error
+    print("deny")
+    pass
